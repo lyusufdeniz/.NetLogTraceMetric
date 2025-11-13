3c77015d401f5bc3bb45686917409a81124aacf2
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-13 23:26:37 +0300

    create tracer provider

--- LogTraceMetric.ConsoleApp/Program.cs
+++ LogTraceMetric.ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using LogTraceMetric.ConsoleApp;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+//create tracer provider
+var traceProvider = Sdk.CreateTracerProviderBuilder().
+    ConfigureResource(conf => conf.AddService(OpenTelemetryConstant.ServiceName, serviceVersion: OpenTelemetryConstant.ServiceVersion).
+    AddAttributes(new List<KeyValuePair<string,object>>()
+    {
+        // seperate words with dot(.) not underscore(_) e.g. host.machineName
+        new KeyValuePair<string, object>("host.machineName", Environment.MachineName),
+        new KeyValuePair<string, object>("host.environment", "dev"),
+
+    })).Build();
