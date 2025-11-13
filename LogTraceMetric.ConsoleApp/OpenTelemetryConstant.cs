fb751667e969bcbb27908ad43eeb6f9b93e0770d
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-11-13 23:26:26 +0300

    create OpenTelemetryConstants

---
+++ LogTraceMetric.ConsoleApp/OpenTelemetryConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace LogTraceMetric.ConsoleApp
+{
+    public class OpenTelemetryConstant
+    {
+       public const string ServiceName = "LogTraceMetric.ConsoleApp";
+        public const string ServiceVersion = "1.0.0";
+
+
+    }
+}
